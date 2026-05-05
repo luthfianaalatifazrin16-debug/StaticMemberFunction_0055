318bedd31bb6674f5b09e480bc7dc805164d7f05
luthfianaalatifazrin16-debug <luthfianaalatifazrin16@gmail.com>
2026-05-05 17:32:08 +0700

    beri value ke variable nim dari class mahasiswa

--- staticmember.cpp
+++ staticmember.cpp
@@ -24,16 +24,3 @@ public:
 
 // memberi value ke variabel nim dari class mahasiswa
 int Mahasiswa::nim = 10;
-
-// mengimplementasikan prosedur - prosedur di luar class
-void Mahasiswa::setID()
-{
-    id = ++nim;
-}
-
-void Mahasiswa::printAll()
-{
-    cout << "ID : " << id << endl;
-    cout << "Nama : " << nama << endl;
-    cout << endl;
-}
