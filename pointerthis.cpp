43836a717674b70c4e9d53e13afe4b9911101b95
luthfianaalatifazrin16-debug <luthfianaalatifazrin16@gmail.com>
2026-05-05 17:39:20 +0700

    class

--- pointerthis.cpp
+++ pointerthis.cpp
@@ -7,22 +7,4 @@ class mahasiswa
 private:
     int nim;
     string nama;
-
-public:
-    // Penggunaan this untuk membedakan variabel member dan parameter
-    void setNim(int nim)
-    {
-        this->nim = nim;
-    }
-
-    void setNama(string nama)
-    {
-        this->nama = nama;
-    }
-
-    void cetak()
-    {
-        cout << "Nim = " << nim << endl;
-        cout << "Nama = " << nama << endl;
-    }
 };
