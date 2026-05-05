e176b322db4ea7e645b0d3da430adf88c99bf5da
luthfianaalatifazrin16-debug <luthfianaalatifazrin16@gmail.com>
2026-05-05 16:51:38 +0700

    chainingfunction

---
+++ chainingfunction.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa
+{
+private:
+    int nim;
+    string nama;
+
+public:
+    mahasiswa &setNim(int nim);
+    mahasiswa &setNama(string nama);
+    void cetak();
+};
+
+mahasiswa &mahasiswa::setNim(int nim)
+{
+    this->nim = nim;
+    return *this;
+}
+
+mahasiswa &mahasiswa::setNama(string nama)
+{
+    this->nama = nama;
+    return *this;
+}
+
+void mahasiswa::cetak()
+{
+    cout << "Nim = " << nim << endl;
+    cout << "Nama = " << nama << endl;
+}
+
+int main()
+{
+    mahasiswa mhs;
+    // Chaining function call
+    mhs.setNama("Huda").setNim(101);
+    mhs.cetak();
+
+    return 0;
+}
